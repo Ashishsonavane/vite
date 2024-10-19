d001426ea032909f77080a640fde84f619ed6522
ASHISH SONAVANE <90111868+Ashishsonavane@users.noreply.github.com>
2024-10-20 04:02:52 +0530

    Update nested.js

--- playground/html/nested/nested.js
+++ playground/html/nested/nested.js
@@ -2,3 +2,38 @@ import { msg } from '../shared'
 import '../common.css'
 
 console.log(msg + ' from nested')
+[04:00:02.044] Running build in Washington, D.C., USA (East) – iad1
+[04:00:02.202] Cloning gitlab.com/sonavaneashish33/nextjs (Branch: main, Commit: 5ffbb52)
+[04:00:02.559] Previous build cache not available
+[04:00:03.460] Cloning completed: 1.257s
+[04:00:03.786] Running "vercel build"
+[04:00:04.207] Vercel CLI 37.9.0
+[04:00:04.619] Installing dependencies...
+[04:00:07.677] npm warn deprecated rimraf@3.0.2: Rimraf versions prior to v4 are no longer supported
+[04:00:07.981] npm warn deprecated inflight@1.0.6: This module is not supported, and leaks memory. Do not use it. Check out lru-cache if you want a good and tested way to coalesce async requests by a key value, which is much more comprehensive and powerful.
+[04:00:08.947] npm warn deprecated @humanwhocodes/object-schema@2.0.3: Use @eslint/object-schema instead
+[04:00:09.031] npm warn deprecated @humanwhocodes/config-array@0.13.0: Use @eslint/config-array instead
+[04:00:09.054] npm warn deprecated glob@7.2.3: Glob versions prior to v9 are no longer supported
+[04:00:10.607] npm warn deprecated eslint@8.57.1: This version is no longer supported. Please see https://eslint.org/version-support for other options.
+[04:00:15.613] 
+[04:00:15.613] added 362 packages in 11s
+[04:00:15.614] 
+[04:00:15.614] 137 packages are looking for funding
+[04:00:15.614]   run `npm fund` for details
+[04:00:15.648] Detected Next.js version: 14.2.15
+[04:00:15.652] Running "npm run build"
+[04:00:15.792] 
+[04:00:15.793] > nextjs@0.1.0 build
+[04:00:15.793] > next build
+[04:00:15.793] 
+[04:00:16.461] Attention: Next.js now collects completely anonymous telemetry regarding usage.
+[04:00:16.462] This information is used to shape Next.js' roadmap and prioritize features.
+[04:00:16.462] You can learn more, including how to opt-out if you'd not like to participate in this anonymous program, by visiting the following URL:
+[04:00:16.462] https://nextjs.org/telemetry
+[04:00:16.463] 
+[04:00:16.528]   ▲ Next.js 14.2.15
+[04:00:16.528] 
+[04:00:16.601]    Creating an optimized production build ...
+[04:00:29.978]  ✓ Compiled successfully
+[04:00:29.979]    Linting and checking validity of types ...
+[04:00:34.467]    Collecting page data ...
