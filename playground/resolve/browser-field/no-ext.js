990b2938a2d5d732636e355989d8f67e8130d3b4
ASHISH SONAVANE <90111868+Ashishsonavane@users.noreply.github.com>
2024-10-20 04:06:20 +0530

    Update no-ext.js

--- playground/resolve/browser-field/no-ext.js
+++ playground/resolve/browser-field/no-ext.js
@@ -1,2 +1,29 @@
 import jsdom from 'jsdom' // should be redirected to empty module
 export default ''
+npm install -D tailwindcss
+npx tailwindcss init
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+npx tailwindcss -i ./src/input.css -o ./src/output.css --watch
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="./output.css" rel="stylesheet">
+</head>
+<body>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</body>
+</html>
